07ec98979d47f203a9ab6d52c6e76afb5ffe1c0a
Psrhiasrcoinlla <43026056+Psrhiasrcoinlla@users.noreply.github.com>
2018-09-27 12:21:33 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+# using function
+
+a=int(input("enter a number:"))
+b=110
+
+
+
+
+
+
+
+
+def addition():
+	return a+b
+
+
+def addition():
+	print("substraction=" ,a-b)
+	return
+
+print("addition=")
+x=add()
+print(x)
+sub()
